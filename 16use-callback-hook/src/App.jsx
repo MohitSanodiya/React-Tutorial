a4a8c4f1ad2352b9107dc8a60d2401d45950fb4a
Mohit Sanodiya <sanodiyamohit1424@gmail.com>
2025-07-10 22:26:37 +0530

    Code Updated

--- 16use-callback-hook/src/App.jsx
+++ 16use-callback-hook/src/App.jsx
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import ChildComponent from './components/ChildComponent';
+import { useCallback } from 'react';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  function Increment(){
+  const handleClick = useCallback(() => {
     setCount(count+1);
-  }
+  }, [count]);
 
 
   return (
     <div className='box'>
       <p>Count : {count}</p>
-
-      <button onClick={Increment}>Increment</button>
+      <button onClick={handleClick}>Increment</button>
       <br />
       <br />
-      <ChildComponent buttonName="Click Me"/>
+      <ChildComponent buttonName="Click Me" handleClick={handleClick}/>
     </div>
 
   )
